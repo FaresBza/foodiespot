8dcff405f9fb70586c5e9203690e52cbf213f04b
FaresBza <fares.bouzaria@crea-inseec.com>
2026-02-11 16:07:34 +0100

    (feat): Update mock menu data with new dishes and images

--- foodiespot/services/api.ts
+++ foodiespot/services/api.ts
@@ -137,47 +137,189 @@ const mockMenus: Record<string, Dish[]> = {
             resurantId: 'r1',
             name: 'Boeuf Bourguignon',
             description: 'Boeuf mijoté au vin rouge avec légumes de saison',
-            price: 18.50,
-            image: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            price: 18.5,
+            image:
+                'https://plus.unsplash.com/premium_photo-1726776138836-6ba2de99c2e7?q=80&w=1902&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             category: 'Plats',
             isAvailable: true,
         },
         {
             id: 'd2',
             resurantId: 'r1',
-            name: 'Tarte Tatin',
-            description: 'Tarte aux pommes caramélisées, servie tiède',
-            price: 7.00,
-            image: 'https://images.unsplash.com/photo-1475856034132-47f1b0752b30?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            name: 'Tarte Framboises',
+            description: 'Tarte aux framboises sauvages',
+            price: 7.0,
+            image:
+                'https://images.unsplash.com/photo-1666418918306-57a80e04bab9?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             category: 'Desserts',
             isAvailable: true,
-        }
+        },
+        {
+            id: 'd5',
+            resurantId: 'r1',
+            name: "Soupe à l'oignon gratinée",
+            description: "Soupe traditionnelle, croûtons et fromage gratiné",
+            price: 8.5,
+            image:
+                'https://plus.unsplash.com/premium_photo-1727960325953-ef51e51d73f1?q=80&w=1333&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Entrées',
+            isAvailable: true,
+        },
     ],
+
     r2: [
         {
             id: 'd3',
             resurantId: 'r2',
             name: 'California Roll',
             description: 'Rouleau de sushi avec crabe, avocat et concombre',
-            price: 12.00,
-            image: 'https://images.unsplash.com/photo-1562967916-eb82221dfb36?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            price: 12.0,
+            image:
+                'https://plus.unsplash.com/premium_photo-1664472644125-f12aecccdd52?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             category: 'Sushi',
             isAvailable: true,
-        }
+        },
+        {
+            id: 'd6',
+            resurantId: 'r2',
+            name: 'Nigiri (12 pcs)',
+            description: 'Nigiri au saumon frais, riz vinaigré',
+            price: 13.5,
+            image:
+                'https://plus.unsplash.com/premium_photo-1723662039343-1d25b547210b?q=80&w=2493&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Sushi',
+            isAvailable: true,
+        },
+        {
+            id: 'd7',
+            resurantId: 'r2',
+            name: 'Sashimi Mix (12 pcs)',
+            description: 'Assortiment de sashimi saumon, thon, daurade',
+            price: 18.0,
+            image:
+                'https://images.unsplash.com/photo-1638866381709-071747b518c8?q=80&w=2312&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Sashimi',
+            isAvailable: true,
+        },
+        {
+            id: 'd8',
+            resurantId: 'r2',
+            name: 'Sushi Mix',
+            description: 'Assortiment de plusieurs sushis',
+            price: 14.5,
+            image:
+                'https://images.unsplash.com/photo-1553621042-f6e147245754?q=80&w=1925&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Poke Bowls',
+            isAvailable: true,
+        },
+        {
+            id: 'd9',
+            resurantId: 'r2',
+            name: 'Gyoza Poulet (6 pcs)',
+            description: 'Raviolis japonais grillés, sauce ponzu',
+            price: 7.5,
+            image:
+                'https://images.unsplash.com/photo-1664138218128-2dcf791a9d27?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Entrées',
+            isAvailable: true,
+        },
     ],
+
+    r3: [
+        {
+            id: 'd10',
+            resurantId: 'r3',
+            name: 'Margherita',
+            description: 'Sauce tomate, mozzarella, basilic',
+            price: 10.0,
+            image:
+                'https://images.unsplash.com/photo-1574071318508-1cdbab80d002?q=80&w=2338&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Pizzas',
+            isAvailable: true,
+        },
+        {
+            id: 'd11',
+            resurantId: 'r3',
+            name: 'Reine',
+            description: 'Sauce tomate, mozzarella, jambon, champignons',
+            price: 12.0,
+            image:
+                'https://images.unsplash.com/photo-1590947132387-155cc02f3212?q=80&w=2070&auto=format&fit=crop',
+            category: 'Pizzas',
+            isAvailable: true,
+        },
+        {
+            id: 'd12',
+            resurantId: 'r3',
+            name: 'Pepperoni',
+            description: 'Base blanche, mozzarella, gorgonzola, chèvre, parmesan',
+            price: 13.5,
+            image:
+                'https://images.unsplash.com/photo-1628840042765-356cda07504e?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Pizzas',
+            isAvailable: true,
+        },
+        {
+            id: 'd13',
+            resurantId: 'r3',
+            name: 'Diavola',
+            description: 'Sauce tomate, mozzarella, salami piquant, olives',
+            price: 14.0,
+            image:
+                'https://images.unsplash.com/photo-1604068549290-dea0e4a305ca?q=80&w=2070&auto=format&fit=crop',
+            category: 'Pizzas',
+            isAvailable: true,
+        },
+    ],
+
     r4: [
         {
             id: 'd4',
-            resurantId: 'r2',
+            resurantId: 'r4',
             name: 'Poke Bowl Saumon',
-            description: 'Bol de riz avec saumon mariné, légumes frais et sauce soja',
-            price: 14.00,
-            image: 'https://images.unsplash.com/photo-1562967916-eb82221dfb36?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            description: 'Riz, saumon cru, avocat, concombre, edamame, algues, sauce soja',
+            price: 14.0,
+            image:
+                'https://plus.unsplash.com/premium_photo-1698867575026-6b96ce023119?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             category: 'Poke Bowls',
             isAvailable: true,
-        }
-    ]
-}
+        },
+        {
+            id: 'd14',
+            resurantId: 'r4',
+            name: 'Poke Bowl Poulet Teriyaki',
+            description: 'Riz, poulet teriyaki, carottes, chou rouge, sésame, sauce maison',
+            price: 13.5,
+            image:
+                'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Poke Bowls',
+            isAvailable: true,
+        },
+        {
+            id: 'd15',
+            resurantId: 'r4',
+            name: 'Poke Bowl Végétarien',
+            description: 'Riz, tomates, brocolis, cèleri, chou, champingnons, sauce soja-sésame',
+            price: 12.5,
+            image:
+                'https://plus.unsplash.com/premium_photo-1698867576608-f8dbde8ecbe8?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Poke Bowls',
+            isAvailable: true,
+        },
+        {
+            id: 'd16',
+            resurantId: 'r4',
+            name: 'Poke Bowl Crevettes',
+            description: 'Riz, crevettes, edamame, ananas, oignons frits, sauce spicy mayo',
+            price: 14.5,
+            image:
+                'https://images.unsplash.com/photo-1602881917760-7379db593981?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            category: 'Poke Bowls',
+            isAvailable: true,
+        },
+    ],
+};
+
 
 // APIs
 export const restaurantAPI = {
